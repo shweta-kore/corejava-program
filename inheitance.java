63eb34692df87927c8234ddc95347f14516aa3de
shweta kore <koreshweta03@gmail.com>
2025-01-22 20:16:32 +0530

    Add files via upload

---
+++ inheitance.java
@@ -0,0 +1,39 @@
+ANP_D0453;
+
+class Person{
+	String name;
+	int age;
+	
+	public Person(String name,int age) {
+		this.name=name;
+		this.age=age;
+	}
+	public void speak() {
+		System.out.println(name+" is speaking.");
+	}
+}
+ class Student extends Person{
+	 String grade;
+	 
+	 public Student(String name,int age,String grade) {
+		 super(name,age);
+		 this.grade=grade;
+		 
+	 }
+	 public void study() {
+		 System.out.println(name+ " is studying in grade" + grade+".");
+	 }
+
+}
+
+public class InheritaceEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student student = new Student("shweta",20," s");
+		student.speak();
+		student.study();
+		
+}
+}
+    
